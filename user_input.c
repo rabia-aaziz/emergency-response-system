2d23bb782de10a589a4431120dff5a5cc28060d0
rabia-aaziz <39001798+rabia-aaziz@users.noreply.github.com>
2026-05-02 22:17:43 +0500

    Update user_input.c

--- user_input.c
+++ user_input.c
@@ -1,1 +1,17 @@
-
+void inputUserInfo(struct Emergency *e) {
+    printf("\n--- Member 2: User Input Module ---");
+    printf("\nEnter Patient/Victim Name: ");
+    
+    // Clear buffer
+    while(getchar() != '\n');
+    
+    // Logic: Scanset to accept spaces in strings
+    scanf("%[^\n]s", e->name); 
+    
+    printf("Enter Contact Number (Format: 03xx-xxxxxxx): ");
+    scanf("%s", e->phone);
+    
+    printf("Enter Live Address (Area, City in Pakistan): ");
+    while(getchar() != '\n');
+    scanf("%[^\n]s", e->address);
+}
